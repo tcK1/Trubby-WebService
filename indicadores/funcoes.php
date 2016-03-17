e26c2a761347860c7539e5ae562c56c101d08b6f
Lucas Delboni <lucas_cd460@hotmail.com>
2016-03-17 01:47:59 +0000

    feito a funcao para retornar quanto gastoude tal item do estoque em tanto tempo

--- indicadores/funcoes.php
+++ indicadores/funcoes.php
@@ -1,6 +1,7 @@
 <?php
     //funcao para retornar quanto de tal item foi vendido em tanto tempo
     function quantidadeProdutoVendidoTempo($nomeProduto, $idUsuario,  $dataInicial, $dataFinal){
+         $BD = new mysqli('54.207.22.190:3306', 'trubby', 'raiztrubby', 'trubby');
         $quantidade =0;
         $sql = "SELECT SUM( `quantidade` ) FROM `vendas_itens` WHERE `id_venda` IN (
                 SELECT `id_venda` FROM `vendas` WHERE `id_usuario` = '$idUsuario' AND `data` 
@@ -8,17 +9,39 @@
                 SELECT `id_produto` FROM `produto` WHERE `nome` = '$nomeProduto')";
         //echo $sql."<br>";
         
-        $BD = new mysqli('54.207.22.190:3306', 'trubby', 'raiztrubby', 'trubby');
+       
         $query = $BD->query($sql);
         while ($dados = $query->fetch_assoc()) {
-          $quantidade = $dados['SUM( `quantidade` )'];
+            $quantidade = $dados['SUM( `quantidade` )'];
         }
+        mysqli_close($BD);
         return $quantidade;
     }
     
     //funcao para retornar quanto gastou de tal item do estoque em tanto tempo
-    function ($nomeEstoque, $dataInicial, $dataFinal, $BD){
+    function quantidadeEstoqueGastoTempo($nomeEstoque, $idUsuario, $dataInicial, $dataFinal){
         $quantidade =0;
+        $BD = new mysqli('54.207.22.190:3306', 'trubby', 'raiztrubby', 'trubby');
+        //primeiro ve quais produtos usam aquele item do estoque
+        $sql = "SELECT `id_ficha` , `quantidade_brt` FROM `ingredientes_uso` WHERE `id_estoque` = (
+                SELECT `id_produto` FROM `produto` WHERE `nome` = '$nomeEstoque' AND `id_produto`IN (
+                SELECT `id_produto` FROM `estoque`WHERE `id_usuario` ='$idUsuario'))";
+        $query = $BD->query($sql);
+        
+        //chama a funcao quantidadeProdutoVendidoTempo para todos esses produtos
+        while ($dados = $query->fetch_assoc()) {
+            $idProduto = $dados['id_ficha'];
+            $sql = "SELECT SUM( `quantidade` ) FROM `vendas_itens` WHERE `id_venda` IN (
+                    SELECT `id_venda` FROM `vendas` WHERE `id_usuario` = '$idUsuario' AND `data` 
+                    BETWEEN '$dataInicial' AND '$dataFinal') AND `id_produto` = $idProduto";
+            $query2 = $BD->query($sql);
+            while ($dados2 = $query2->fetch_assoc()) {
+                $produtosVendidos = $dados2['SUM( `quantidade` )'];
+                $quantidade = $quantidade + $produtosVendidos*$dados['quantidade_brt'];
+            }
+            
+        }
+        mysqli_close($BD);
         return $quantidade;
     }
 ?>
---
+++ indicadores/index.php
@@ -0,0 +1,10 @@
+<?php
+    
+    include "funcoes.php";
+
+    $quantidade = quantidadeProdutoVendidoTempo('abobora', 10,  '0000-00-00 00:00:00', '1000-00-00 00:00:00');
+    echo "INDEX - quantidade: ".$quantidade."<br>";
+    
+    $quantidadeEstoque = quantidadeEstoqueGastoTempo('conhaque', 10, '0000-00-00 00:00:00', '1000-00-00 00:00:00');
+    echo "INDEX - estoque: ".$quantidadeEstoque."<br>";
+?>
